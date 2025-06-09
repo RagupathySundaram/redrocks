4c8268d4d4c3a42b31b2afb569183c3add33c205
Ragupathy Sundaram <ragupathys26@gmail.com>
2025-06-09 22:59:02 +0530

    Pushing build to repo

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Server running on port ${PORT}`);
 });
